3beca987821fe928b65681abe6c788a0a699f4af
YounTaeWoo <0lisa@naver.com>
2025-03-10 17:22:18 +0900

    feat : 카드 브랜드로 조회 생성

--- src/main/java/card_recommend_project/card_recommend_project/CardQueryRepository.java
+++ src/main/java/card_recommend_project/card_recommend_project/CardQueryRepository.java
@@ -1,8 +1,10 @@
 package card_recommend_project.card_recommend_project;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -18,5 +20,14 @@ public class CardQueryRepository {
 
     public List<Card> findAll() {
         return null;
+    }
+
+    public List<BooleanExpression> findByKeyWord(List<String> cardBrands) {
+        if (cardBrands == null) {
+            return null;
+        }
+        return cardBrands.stream()
+                .map(cardBrand -> card.cardBrand.eq(cardBrand))
+                .toList();
     }
 }
