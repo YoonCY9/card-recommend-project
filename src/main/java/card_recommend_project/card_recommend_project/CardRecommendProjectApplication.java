cce41ee4658c6c9e7af32c802ba5cadeb9e924b3
TJOEUNIS <HwaEVE@gmail.com>
2025-03-10 12:02:43 +0900

    의존성 추가

---
+++ src/main/java/card_recommend_project/card_recommend_project/CardRecommendProjectApplication.java
@@ -0,0 +1,13 @@
+package card_recommend_project.card_recommend_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardRecommendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardRecommendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/card_recommend_project/card_recommend_project/CardRecommendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package card_recommend_project.card_recommend_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardRecommendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
